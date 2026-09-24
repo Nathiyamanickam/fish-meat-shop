99bc810c9825cfe216351a6be119d8d804acec80
Nathiya <nathiyamanickam6181@gmail.com>
2026-09-24 11:45:17 +0530

    Make Razorpay optional for deployment

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,8 +1,21 @@
 const Razorpay = require("razorpay");
 
-const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
+let razorpay = null;
+
+if (
+  process.env.RAZORPAY_KEY_ID &&
+  process.env.RAZORPAY_KEY_SECRET
+) {
+  razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+  });
+
+  console.log("Razorpay configured successfully");
+} else {
+  console.log(
+    "Razorpay keys not configured. Payment feature is disabled."
+  );
+}
 
 module.exports = razorpay;
